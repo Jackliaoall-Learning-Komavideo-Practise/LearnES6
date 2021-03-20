97048e2457c5afe9537c4df539976559f9b6be85
jackliaoall <jackliaoall@gmail.com>
2021-03-20 15:22:36 +0800

    first commit

---
+++ Lesson02/test.js
@@ -0,0 +1,10 @@
+var i = 0;
+
+switch(i) {
+  case 0:
+    let value = "hello";
+    break;
+  case 1:
+    let value = "world";
+    break;
+}
---
+++ Lesson03/test.js
@@ -0,0 +1,8 @@
+const data = 10;
+console.log(data);
+// data = 100; //Error
+
+const list = [10,20,30];
+list[0] = 100;
+console.log(list);
+// list = [1,2,3]; // Error
